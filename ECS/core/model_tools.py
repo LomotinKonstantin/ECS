db4d7337275bbaded30ca170ec02036a1e863716
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-09-02 16:40:51 +0300

    Добавлены функции для тестирования и генерации отчета
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- ECS/core/model_tools.py
+++ ECS/core/model_tools.py
@@ -1,12 +1,22 @@
 import warnings
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 from importlib import import_module
 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
-from sklearn.metrics import classification_report
+from sklearn.metrics import \
+    confusion_matrix, \
+    accuracy_score, \
+    precision_score, \
+    recall_score, \
+    f1_score
 from joblib import dump, load
+import pandas as pd
+import numpy as np
+
+
+from ECS.core.codes_helper import RubricManager
 
 
 def load_class(classpath: str) -> type:
@@ -76,23 +86,157 @@ def refit_model(model_instance, best_params: dict,
     return model_instance
 
 
-def save_model(model, path: str) -> None:
+def save_model(model, path: str, metadata: dict) -> None:
     """
     Сохранить модель в файл.
     Функция используется для абстрагирования от используемого метода.
     Логичнее было бы использовать hdf5 или pickle,
     но в АТС почему-то используется joblib.
+    :param metadata: словарь с дополнительной информацией
     :param model: объект модели
     :param path: путь к файлу для сохранения
     :return: None
     """
-    dump(model, path)
+    with open(path, "wb") as file:
+        dump(model, file)
+        dump(metadata, file)
 
 
-def load_model(path: str):
+def load_model(path: str) -> tuple:
     """
     Загрузить модель из файла
     :param path: путь к модели
-    :return: объект модели
+    :return: кортеж (объект_модели, словарь_метадаты)
+             Если модель старой версии и не содержит метадаты,
+             на втором месте будет None
     """
-    return load(path)
+    metadata = None
+    with open(path, "rb") as file:
+        model = load(file)
+        try:
+            metadata = load(file)
+        except EOFError:
+            pass
+    return model, metadata
+
+
+def create_report(model, x_test: list, y_test: list):
+    # Для обратной совместимости используется старый код
+    pred = []
+    for p in model.predict_proba(x_test):
+        all_prob = pd.Series(p, index=model.classes_)
+        pred.append(list(all_prob.sort_values(ascending=False).index))
+    return count_stats(predicts=pred, y_test=y_test,
+                       amounts=[1, 2, 3, 4, 5, -1])
+
+
+#################################################################
+#                                                               #
+#   ACHTUNG! Ниже находится Территория Легаси.                  #
+#   Тут был проведен лишь легкий косметический рефакторинг,     #
+#   чтобы не ругался линтер. На этой заповедной территории      #
+#   сохранены даже старые комментарии. Возможно, однажды это    #
+#   будет  переписано. Кто знает, кто знает...                  #
+#                                                               #
+#################################################################
+def count_stats(predicts: list, y_test: list,
+                amounts: list):
+    """
+    Counts statistics for predictions of a classifier
+
+    Args:
+    predicts    -- classifiers answers for X_test.
+    y_test      -- real rubrics of X_test.
+    legend      -- list with ordered unique rubrics. If equals to None, legend will be created in alphabet order.
+    amounts     -- list with amounts of answers we want to test (-1 means all answers). 1 by default.
+    version     -- int or str with version of file. 1 by default.
+
+    Возвращает словарь датафреймов. Ключи - '1'...'5' и 'all'
+    """
+    legend = [item for sublist in predicts for item in sublist]
+    legend = pd.Series(map(str, legend))
+    legend = legend.unique()
+    legend.sort()
+    legend = list(legend)
+    if -1 not in amounts:
+        amounts = list(map(int, amounts))
+        amounts.sort()
+        amounts = amounts[::-1]
+    else:
+        amounts = list(set(amounts) - {-1})
+        amounts.sort()
+        amounts = [-1] + amounts[::-1]
+    keys, values = [], []
+    for a in amounts:
+        k = []
+        if a != -1:
+            for j in predicts:
+                k += [j[:a]]
+        else:
+            k = predicts
+        cur_pred = k
+        stats = pd.DataFrame(columns=['accuracy', 'precision', 'recall', 'f1-score', 'TP', 'FP', 'FN', 'TN'])
+        for i in legend:
+            cur_predicts = []
+            cur_y_test = []
+            for j in zip(cur_pred, y_test):
+                if (type(j[0]) == list and i in j[0]) or i == j[0]:
+                    cur_predicts += [1]
+                else:
+                    cur_predicts += [0]
+                if (type(j[1]) == list and i in j[1]) or i == j[1]:
+                    cur_y_test += [1]
+                else:
+                    cur_y_test += [0]
+            temp = []
+            for l in binary_metrics(cur_predicts, cur_y_test):
+                temp += [l]
+            mat = confusion_matrix(cur_predicts, cur_y_test)
+            if mat.shape == (1, 1):
+                conf_matrix = [0, 0, 0] + list(np.array(mat).ravel())
+            else:
+                conf_matrix = list(np.array(mat).ravel())[::-1]
+            stats = stats.append(pd.DataFrame([temp + conf_matrix],
+                                              columns=['accuracy', 'precision', 'recall',
+                                                       'f1-score', 'TP', 'FP', 'FN', 'TN'], index=[i]))
+        stats = stats.sort_index()
+        stats_mean = stats.mean().values
+        tp, fp, fn, tn = stats_mean[4:]
+        acc_temp = (tp + tn) / (tp + fp + fn + tn)
+        pr_temp = tp / (tp + fp)
+        rec_temp = tp / (tp + fn)
+        f1_temp = 2 * pr_temp * rec_temp / (pr_temp + rec_temp)
+        stats = stats.append(pd.DataFrame([list(stats_mean[0:4]) + ['-'] * 4],
+                                          columns=['accuracy', 'precision', 'recall', 'f1-score',
+                                                   'TP', 'FP', 'FN', 'TN'],
+                                          index=['macro']))
+        stats = stats.append(pd.DataFrame([[acc_temp, pr_temp, rec_temp, f1_temp] + list(stats_mean[4:])],
+                                          columns=['accuracy', 'precision', 'recall', 'f1-score',
+                                                   'TP', 'FP', 'FN', 'TN'],
+                                          index=['micro']))
+        if a != -1:
+            keys += [str(a)]
+        else:
+            keys += ['all']
+        values += [stats]
+    full_stats = dict(zip(keys, values))
+    return full_stats
+
+
+def binary_metrics(predicts, y_test):
+    """
+    Counts binary accuracy, precision, recall and f1.
+
+    Args:
+    predicts    -- classifiers answers for X_test (0 and 1 for a particular rubric).
+    y_test      -- real rubrics of X_test (0 and 1 for a particular rubric).
+    """
+    ac = accuracy_score(y_test, predicts)
+    pr = precision_score(y_test, predicts)
+    rec = recall_score(y_test, predicts)
+    f1 = f1_score(y_test, predicts)
+    return [ac, pr, rec, f1]
+
+
+if __name__ == '__main__':
+    trained_model = load_model(r"D:\Desktop\VINITI\ECS\test\unit_tests\test_data\test_model.plk")
