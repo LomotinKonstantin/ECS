25780428e5ae4961760f0f25c01056eca682e3fc
Konstantin Lomotin <ke.lomotin@gmail.com>
2020-01-15 18:19:55 +0300

    Обновлен интерфейс
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- ECS/core/models/abstract_model.py
+++ ECS/core/models/abstract_model.py
@@ -1,9 +1,19 @@
+from ECS.core.model_tools import load_class
+
+
 class AbstractModel:
+
+    def __init__(self, classpath: str):
+        """
+            :raise ImportError
+        """
+        self.class_type = load_class(classpath)
+        self.instance = None
 
     def load(self, path: str):
         pass
 
-    def save(self, path: str):
+    def save(self, path: str, metadata: dict) -> None:
         pass
 
     def predict(self, data):
@@ -12,11 +22,10 @@ class AbstractModel:
     def predict_proba(self, data):
         pass
 
-    '''
-    Включает в себя refit
-    '''
-    def run_grid_search(self):
+    def run_grid_search(self, **kwargs):
         pass
 
-    def __load_class(self, path: str):
+    def refit(self, best_params: dict, x_train: list,
+              y_train: list, binary: bool):
         pass
+
